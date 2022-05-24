d51a51e5518bdd9c334999d4d0a3c366d9a0f94c
bluuweb <contacto@bluuweb.cl>
2022-05-24 11:55:02 -0400

    🐱‍👤🐱‍👤

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,38 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { nanoid } from "nanoid";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {},
-    mutations: {},
-    actions: {},
+    state: {
+        desserts: [],
+    },
+    mutations: {
+        ADD_DESSERT(state, payload) {
+            state.desserts.push(payload);
+        },
+        DELETE_DESSERT(state, payload) {
+            state.desserts = state.desserts.filter(
+                (item) => item.id !== payload
+            );
+        },
+        UPDATE_DESSERT(state, payload) {
+            state.desserts = state.desserts.map((item) =>
+                item.id === payload.id ? payload : item
+            );
+        },
+    },
+    actions: {
+        add_dessert({ commit }, dessert) {
+            dessert.id = nanoid(6);
+            commit("ADD_DESSERT", dessert);
+        },
+        delete_dessert({ commit }, id) {
+            commit("DELETE_DESSERT", id);
+        },
+        update_dessert({ commit }, dessert) {
+            commit("UPDATE_DESSERT", dessert);
+        },
+    },
 });
--- src/views/Crud.vue
+++ src/views/Crud.vue
@@ -15,12 +15,12 @@
                     <v-col cols="12" md="6">
                         <v-text-field
                             required
-                            label="Name"
+                            label="Calories"
                             v-model="dessert.calories"
                         />
                     </v-col>
                 </v-row>
-                <v-btn block color="primary">ADD</v-btn>
+                <v-btn block color="primary" @click="addDessertForm">ADD</v-btn>
             </v-container>
         </v-form>
 
@@ -38,26 +38,93 @@
                         <td>{{ item.name }}</td>
                         <td>{{ item.calories }}</td>
                         <td class="text-center">
-                            <v-btn class="mr-2" color="error">Eliminar</v-btn>
-                            <v-btn color="warning">Editar</v-btn>
+                            <v-btn
+                                class="mr-2"
+                                color="error"
+                                @click="deleteDessertItems(item.id)"
+                                >Eliminar</v-btn
+                            >
+                            <v-btn color="warning" @click="activarUpdate(item)"
+                                >Editar</v-btn
+                            >
                         </td>
                     </tr>
                 </tbody>
             </template>
         </v-simple-table>
+
+        <v-dialog v-model="dialog">
+            <v-card>
+                <v-card-title class="text-h5 grey lighten-2">
+                    Editar
+                </v-card-title>
+                <v-card-text>
+                    <v-form>
+                        <v-container>
+                            <v-row>
+                                <v-col cols="12" md="6">
+                                    <v-text-field
+                                        required
+                                        label="Name"
+                                        v-model="dessert.name"
+                                    />
+                                </v-col>
+                                <v-col cols="12" md="6">
+                                    <v-text-field
+                                        required
+                                        label="Calories"
+                                        v-model="dessert.calories"
+                                    />
+                                </v-col>
+                            </v-row>
+                            <v-btn
+                                block
+                                color="warning"
+                                @click="updateDessertForm"
+                                >UPDATE</v-btn
+                            >
+                        </v-container>
+                    </v-form>
+                </v-card-text>
+            </v-card>
+        </v-dialog>
     </div>
 </template>
 
 <script>
+import { mapActions, mapState } from "vuex";
+
 export default {
     data() {
         return {
-            desserts: [{ name: "cupcake", calories: "1", id: 1 }],
             dessert: {
                 name: "",
                 calories: "",
             },
+            dialog: false,
         };
+    },
+    methods: {
+        ...mapActions(["add_dessert", "delete_dessert", "update_dessert"]),
+        addDessertForm() {
+            this.add_dessert({ ...this.dessert });
+        },
+        deleteDessertItems(id) {
+            this.delete_dessert(id);
+        },
+        activarUpdate(item) {
+            this.dialog = true;
+            this.dessert = item;
+        },
+        updateDessertForm() {
+            console.log(this.dessert);
+            this.update_dessert({ ...this.dessert });
+            this.dessert = {};
+            this.dialog = false;
+        },
+    },
+    computed: {
+        ...mapState(["desserts"]),
     },
 };
 </script>
