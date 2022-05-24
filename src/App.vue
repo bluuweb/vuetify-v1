dec3ba2afed15493987bdb86d7e8ef92fb61a262
bluuweb <contacto@bluuweb.cl>
2022-05-23 20:45:13 -0400

    fkasdjfio

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,39 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+    <v-app>
+        <v-app-bar app class="orange darken-4" dark>
+            <v-btn color="primary" to="/crud">
+                <span class="mr-2">CRUD</span>
+                <v-icon>mdi-bell-badge</v-icon>
+            </v-btn>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" class="mr-2" to="/register">
+                <span class="mr-2">Register</span>
+                <v-icon>mdi-bell-badge</v-icon>
+            </v-btn>
+            <v-btn color="primary" class="mr-2" to="/login">
+                <span class="mr-2">Login</span>
+                <v-icon>mdi-bell-badge</v-icon>
+            </v-btn>
+            <v-btn color="primary">
+                <span class="mr-2">Logout</span>
+                <v-icon>mdi-bell-badge</v-icon>
+            </v-btn>
+        </v-app-bar>
+
+        <v-main>
+            <v-container>
+                <router-view />
+            </v-container>
+        </v-main>
+    </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+    name: "App",
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+    data: () => ({
+        //
+    }),
+};
+</script>
---
+++ src/views/Crud.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <h1>CRUD</h1>
+
+        <v-form>
+            <v-container>
+                <v-row>
+                    <v-col cols="12" md="6">
+                        <v-text-field
+                            required
+                            label="Name"
+                            v-model="dessert.name"
+                        />
+                    </v-col>
+                    <v-col cols="12" md="6">
+                        <v-text-field
+                            required
+                            label="Name"
+                            v-model="dessert.calories"
+                        />
+                    </v-col>
+                </v-row>
+                <v-btn block color="primary">ADD</v-btn>
+            </v-container>
+        </v-form>
+
+        <v-simple-table>
+            <template v-slot:default>
+                <thead>
+                    <tr>
+                        <th class="text-left">Nombre</th>
+                        <th class="text-left">Calorias</th>
+                        <th class="text-center">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="item in desserts" :key="item.id">
+                        <td>{{ item.name }}</td>
+                        <td>{{ item.calories }}</td>
+                        <td class="text-center">
+                            <v-btn class="mr-2" color="error">Eliminar</v-btn>
+                            <v-btn color="warning">Editar</v-btn>
+                        </td>
+                    </tr>
+                </tbody>
+            </template>
+        </v-simple-table>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            desserts: [{ name: "cupcake", calories: "1", id: 1 }],
+            dessert: {
+                name: "",
+                calories: "",
+            },
+        };
+    },
+};
+</script>
